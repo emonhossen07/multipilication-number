4a8e30c27ca425c568cc6a1dbb0f6f7efffbdb32
ruddro420 <aliruddro4@gmail.com>
2023-07-31 04:04:58 +0600

    ruddro done for today

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 document.getElementById('getNamota').addEventListener('click', function () {
 
+    document.getElementById('no-more').style.display = 'none'
+
     const inputData = document.getElementById('getInput').value;
     const finalInputData = parseInt(inputData)
-    // result show hre
-    let getResult = document.getElementById('resultShow')
-    let final = ''
-    let html = ''
-    for (let i = 1; i <= 10; i++) {
-        final = i * finalInputData
-        html += (`
+
+
+    if (inputData === '') {
+        alert('Field should no empty')
+        document.getElementById('no-more').style.display = 'block'
+    } else {
+
+        // result show hre
+        let getResult = document.getElementById('resultShow')
+        let final = ''
+        let html = ''
+        for (let i = 1; i <= 10; i++) {
+            final = i * finalInputData
+            html += (`
 
             ${finalInputData} × ${i} = ${finalInputData * i + "<br/>"} `);
-        document.getElementById('getInput').value = ''
+            document.getElementById('getInput').value = ''
+
+        }
+        getResult.innerHTML = html
 
     }
-    getResult.innerHTML = html
+
+
+})
+
+/* For redirect page */
+
+
+document.getElementById('redirectPage').addEventListener('click', function () {
+    console.log('click');
+    window.location.href = "pages/main.html";
 })
